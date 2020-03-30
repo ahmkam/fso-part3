3596074736f7db29c79a75c69dbe6c00254334b9
ahmed kamal <ahmedkl29@gmail.com>
2020-03-30 20:34:34 +0300

    build

---
+++ build/precache-manifest.47f6d15a6c2ec41bb27b48c692b264c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569033f6f534459832b31bd052404904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b25d2dd7a47a4ab1c92b",
+    "url": "/static/css/main.2e17fcc3.chunk.css"
+  },
+  {
+    "revision": "c5cf8bd149580aa910cd",
+    "url": "/static/js/2.57bf6790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57bf6790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25d2dd7a47a4ab1c92b",
+    "url": "/static/js/main.b3aa1862.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
